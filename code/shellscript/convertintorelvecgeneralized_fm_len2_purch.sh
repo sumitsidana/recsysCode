b79b03f8bc9fef69e71700d448dd81ae4b4b7631
sumitsidana <sidana.sumit@gmail.com>
2017-04-17 01:33:15 +0200

    add: ranked list for purch

---
+++ code/shellscript/convertintorelvecgeneralized_fm_len2_purch.sh
@@ -0,0 +1,13 @@
+cd ../java/src/
+mkdir -p /data/sidana/purch/len2/rv
+javac preProcess/ConvertIntoRelVecGeneralized.java
+for countryCode in "at" "be" "br" "ch" "cz" "de" "dk" "es" "fi" "ie" "nb" "nl" "no" "pl" "pt" "ru" "se" "uk" "it" "fr"; do
+    start=$(date +%s.%N)
+	filename1="/data/sidana/purch/vectors/gt_$countryCode"
+	filename2="/data/sidana/purch/vectors/pr_$countryCode"
+	filename3="/data/sidana/purch/len2/rv/relevanceVector_$countryCode"
+	java -cp . preProcess.ConvertIntoRelVecGeneralized "$filename1" "$filename2" "$filename3" 15
+    dur=$(echo "$(date +%s.%N) - $start" | bc)
+    echo "$countryCode:$dur">>/data/sidana/purch/len2/rv/convertintorelvecgeneralized.txt
+done
+cd -
---
+++ code/shellscript/relvecfm_purch.sh
@@ -0,0 +1,4 @@
+for (( i=1; i <= 4; i++ ))
+do
+./convertintorelvecgeneralized_fm_len$i_purch.sh
+done
