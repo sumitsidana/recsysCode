41fa74dd4a3c21178e6e47c47a3df7c10d26c6a9
sumitsidana <sidana.sumit@gmail.com>
2017-04-17 00:48:33 +0200

    libfm format purch

--- code/shellscript/triple_format_to_libfm_purch.sh
+++ code/shellscript/triple_format_to_libfm_purch.sh
@@ -1,4 +1,4 @@
-cd ../cplusplus/libfm-1.42.src/scripts
+cd ~/recsysBaselines/code/cplusplus/libfm-1.42.src/scripts
 start=$(date +%s.%N) 
 echo $countryCode
 ./triple_format_to_libfm.pl -in /data/sidana/purch/train.csv,/data/sidana/purch/test.csv -target 2 -separator "\t"
