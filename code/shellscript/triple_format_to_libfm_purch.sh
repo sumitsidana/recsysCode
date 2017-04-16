592530cb7dd50fd4e2367115022fdf7c912e7994
sumitsidana <sidana.sumit@gmail.com>
2017-04-17 00:46:21 +0200

    libfm format purch

---
+++ code/shellscript/triple_format_to_libfm_purch.sh
@@ -0,0 +1,7 @@
+cd ../cplusplus/libfm-1.42.src/scripts
+start=$(date +%s.%N) 
+echo $countryCode
+./triple_format_to_libfm.pl -in /data/sidana/purch/train.csv,/data/sidana/purch/test.csv -target 2 -separator "\t"
+dur=$(echo "$(date +%s.%N) - $start" | bc)
+echo "$countryCode:$dur">>/data/sidana/purch/triple_format_to_libfm.txt
+cd -
