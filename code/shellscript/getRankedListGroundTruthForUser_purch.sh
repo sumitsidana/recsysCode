25047a0188c3aaf53a83394a42800fa9453a9aed
sidana <sidana@tiger.imag.fr>
2017-05-03 23:01:18 +0200

    add shellscripts

--- code/shellscript/getRankedListGroundTruthForUser_purch.sh
+++ code/shellscript/getRankedListGroundTruthForUser_purch.sh
@@ -1,13 +1,13 @@
 cd ../java/src
-mkdir -p /data/sidana/purch/vectors
+mkdir -p /data/sidana/purch/fm/vectors
 javac preProcess/InputOutput.java preProcess/GetRankedListGroundTruthForUser.java
 start=$(date +%s.%N)
-filename1="/data/sidana/purch/test.csv"
-filename2="/data/sidana/purch/libfmresult"
-filename3="/data/sidana/purch/vectors/gt_$countryCode"
-filename4="/data/sidana/purch/vectors/pr_$countryCode"
+filename1="/data/sidana/purch/fm/test"
+filename2="/data/sidana/purch/fm/libfmresult"
+filename3="/data/sidana/purch/fm/vectors/gt_$countryCode"
+filename4="/data/sidana/purch/fm/vectors/pr_$countryCode"
 
 java -cp . preProcess.GetRankedListGroundTruthForUser "$filename1" "$filename2" "$filename3" "$filename4"
 dur=$(echo "$(date +%s.%N) - $start" | bc)
-echo "$countryCode:$dur">>/data/sidana/purch/vectors/getrankedlistgroundtruthforuser.txt
+echo "$countryCode:$dur">>/data/sidana/purch/fm/vectors/getrankedlistgroundtruthforuser.txt
 cd -
--- code/shellscript/libfm_purch.sh
+++ code/shellscript/libfm_purch.sh
@@ -1,6 +1,6 @@
 cd ~/recsysBaselines/code/cplusplus/libfm-1.42.src/bin/
 start=$(date +%s.%N)
-./libFM -task c -train /data/sidana/purch/train.csv.libfm -test /data/sidana/purch/test.csv.libfm -dim '1,1,10' -iter 10 -method sgd -learn_rate 0.001 -regular '0,0,0.01' -init_stdev 0.1 -out /data/sidana/purch/libfmresult -rlog /data/sidana/purch/log
+./libFM -task c -train /data/sidana/purch/fm/train.libfm -test /data/sidana/purch/fm/test.libfm -dim '1,1,10' -iter 10 -method sgd -learn_rate 0.001 -regular '0,0,0.01' -init_stdev 0.1 -out /data/sidana/purch/fm/libfmresult -rlog /data/sidana/purch/fm/log
 dur=$(echo "$(date +%s.%N) - $start" | bc)
-echo "$countryCode:$dur">>/data/sidana/purch/libfm.txt
+echo "$countryCode:$dur">>/data/sidana/purch/fm/libfm.txt
 cd -
