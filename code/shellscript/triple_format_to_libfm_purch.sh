db94a9a7c6619e3448b8e3d225420ac395b04b8f
sidana <sidana@tiger.imag.fr>
2017-05-03 22:50:28 +0200

    add: purch fm

--- code/shellscript/triple_format_to_libfm_purch.sh
+++ code/shellscript/triple_format_to_libfm_purch.sh
@@ -1,7 +1,7 @@
 cd ~/recsysBaselines/code/cplusplus/libfm-1.42.src/scripts
 start=$(date +%s.%N) 
 echo $countryCode
-./triple_format_to_libfm.pl -in /data/sidana/purch/train.csv,/data/sidana/purch/test.csv -target 2 -separator "\t"
+./triple_format_to_libfm.pl -in /data/sidana/purch/fm/train,/data/sidana/purch/fm/test -target 2  -delete_column 3 -separator ","
 dur=$(echo "$(date +%s.%N) - $start" | bc)
 echo "$countryCode:$dur">>/data/sidana/purch/triple_format_to_libfm.txt
 cd -
