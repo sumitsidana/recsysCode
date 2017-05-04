6f62a23335ca1a8670f9a56157802b6dbe66bfb1
sumit <sumit.sidana@imag.fr>
2017-05-04 12:36:10 +0200

    add: i/o

--- code/java/src/preProcess/IndexTrainAndTestFiles.java
+++ code/java/src/preProcess/IndexTrainAndTestFiles.java
@@ -6,6 +6,6 @@ public class IndexTrainAndTestFiles {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		InputOutput.indexTrainAndTestFiles(args[0], args[1]/*, args[2]*/, args[3], args[4]/*, args[5]*/);
+		InputOutput.indexTrainAndTestFiles(args[0], args[1]/*, args[2]*/, args[2], args[3]/*, args[5]*/);
 	}
 }
